d728d5bd4311ddcc19905df7c13d25f72023e58e
nicolastpi01 <nicolastpi10@gmail.com>
2025-12-16 23:02:12 -0300

    esqueleto del proyecto

---
+++ src/main/java/com/aurum/api/AurumApplication.java
@@ -0,0 +1,13 @@
+package com.aurum.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aurum/api/AurumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurum.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
